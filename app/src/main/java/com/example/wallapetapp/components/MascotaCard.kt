bdaf475b650bd07c7c80d6e7f941724f8b7cc6d1
Jessica <jfitemol@myuax.com>
2024-01-30 00:14:26 +0100

    consigo mostrar la foto defecto

--- app/src/main/java/com/example/wallapetapp/components/MascotaCard.kt
+++ app/src/main/java/com/example/wallapetapp/components/MascotaCard.kt
@@ -1,26 +1,34 @@
 package com.example.wallapetapp.components
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.example.wallapetapp.R
 import com.example.wallapetapp.domain.model.Mascota
+import kotlinx.coroutines.NonDisposableHandle.parent
 import java.io.File
+
 
 @Composable
 fun MascotaCard(mascota: Mascota) {
@@ -30,7 +38,7 @@ fun MascotaCard(mascota: Mascota) {
             .padding(start = 8.dp, end = 8.dp, top = 4.dp, bottom = 4.dp)
             .fillMaxWidth(),
         elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
-    ){
+    ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -38,8 +46,19 @@ fun MascotaCard(mascota: Mascota) {
             verticalAlignment = Alignment.CenterVertically
         ) {
             Column {
-                PhotoView(imagePath = mascota.foto)
-                TextoCardDatos(texto = mascota.foto)
+                //PhotoView(imagePath = mascota.foto)
+                //Text(text = mascota.foto)
+                val ctx = LocalContext.current
+                val bitmap = getBitmapFromImage(ctx, R.drawable.fotodefecto)
+                Image(
+                    modifier = Modifier
+                        .height(200.dp)
+                        .width(200.dp),
+                    bitmap = bitmap.asImageBitmap(),
+                    contentDescription = "Android",
+                    alignment = Alignment.Center
+                )
+
                 TextoCardTitulos(texto = stringResource(R.string.nombre))
                 TextoCardDatos(texto = mascota.nombre)
                 TextoCardTitulos(texto = stringResource(R.string.poblacioncard))
@@ -58,23 +77,45 @@ fun MascotaCard(mascota: Mascota) {
 }
 
 @Composable
-fun PhotoView(imagePath: String){
+fun PhotoView(imagePath: String) {
+    Text(text = "Entro en PhotoView")
+    Text(text = imagePath)
+
     val bitmap = remember {
-        try{
+        try {
             val file = File(imagePath)
             BitmapFactory.decodeFile(file.path)
-        }catch (e:Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
             null
         }
     }
-    if(bitmap!=null){
-        Image(bitmap=bitmap.asImageBitmap(),
+    if (bitmap != null) {
+
+        Image(
+            bitmap = bitmap.asImageBitmap(),
             contentDescription = "",
             modifier = Modifier
                 .padding(15.dp)
         )
-    }else{
+    } else {
         Text(text = "Error al cargar la imagen")
     }
+}
+//BitmapFactory.decodeResource(getResources(), R.id.arrow)
+
+private fun getBitmapFromImage(context: Context, drawable: Int): Bitmap {
+
+    val db = ContextCompat.getDrawable(context, drawable)
+
+    val bit = Bitmap.createBitmap(
+        db!!.intrinsicWidth, db.intrinsicHeight, Bitmap.Config.ARGB_8888
+    )
+
+    val canvas = android.graphics.Canvas(bit)
+    db.setBounds(0, 0, canvas.width, canvas.height)
+
+    db.draw(canvas)
+
+    return bit
 }

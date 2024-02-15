18c7cc98ccd14e160b28a33a10e36aeb7c852dd9
Jessica <jfitemol@myuax.com>
2024-02-15 20:16:18 +0100

    cambio notificacion

--- app/src/main/java/com/example/wallapetapp/notificaciones/Notificacion.kt
+++ app/src/main/java/com/example/wallapetapp/notificaciones/Notificacion.kt
@@ -8,8 +8,8 @@ import androidx.core.app.NotificationCompat
 import com.example.wallapetapp.R
 
 class Notificacion(var context: Context){
-    val title = "Gracias"
-    val msg = "Has entrado al sistema una nueva mascota que busca a sus dueños. Gracias por ayudarla"
+    val title = context.getString(R.string.gracias)
+    val msg = context.getString(R.string.por_ayudar_a_encontrar_su_familia_a_la_mascota)
     val channelId = "Canal1"
     val channelName = "Canal Wallapet"
     val notificationManager=context.applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
